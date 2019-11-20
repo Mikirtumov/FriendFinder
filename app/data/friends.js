c9f851f5c7679b1ef2da8f54432dead0a2da3aa9
Sergo Mikirtumov <mikirtumov@gmail.com>
2019-11-20 15:43:08 -0500

    saving

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+// Friends JSON file
+var friends = [
+	{
+	name: "Elon Musk",
+	photo: "https://myrouble.ru/wp-content/uploads/2018/03/ilon-mask-57.jpg",
+	scores: [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+			]
+	},
+	{
+	name: "Jeff Bezos",
+	photo: "https://secure.i.telegraph.co.uk/multimedia/archive/03557/Amazon_Jeff_Bezos_3557348c.jpg",
+	scores: [
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4"
+			]
+	},
+	{
+	name: "Bill Gates",
+	photo: "https://24smi.org/public/media/celebrity/2019/04/16/gamgu2r9o0zm-bill-geits.jpg",
+	scores: [
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3"
+			]
+	},
+	{
+	name: "Pavel Durov",
+	photo: "http://allrus.me/wp-content/uploads/2013/09/Pavel-Durov-5.jpg",
+	scores: [
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2"
+			]
+	},
+	{
+	name: "Stive Jobs",
+	photo: "https://images-na.ssl-images-amazon.com/images/I/61PdVxgNq8L._SX425_.jpg",
+	scores: [
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
+			]
+	}
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,48 @@
+var path = require("path");
+var friendsData = require ("../data/friends");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function(req, res){
+        res.json(friendsData);
+    });
+
+    app.post('/api/friends', function(req, res) {
+        var difference = 40;
+        var matchName = '';
+        var matchPhoto = '';
+
+        friendsData.forEach(function(friends) {
+ 
+            var matchedScoresArray = [];
+            var totalDifference = 40;
+
+            function add(total, num) {
+                return total + num;
+            }
+            
+            for (var i = 0; i < friends.scores.length; i++) {
+                matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friends.scores[i])));
+            }
+
+            totalDifference = matchedScoresArray.reduce(add, 0);
+
+
+            if (totalDifference < difference) {
+
+                difference = totalDifference;
+
+                matchName = friends.name;
+                matchPhoto = friends.photo;
+            }
+    });
+
+    res.json({
+        name: matchName,
+        photo: matchPhoto
+    });
+
+    friendsData.push(req.body);
+});
+};
+
